1f8c4a6a11c9681abee4770631ef405aae39ba58
Vaibhav <vibhu.astute@gmil.com>
2022-10-12 17:40:05 +0530

    Added Download  Button To Certifcate Generator

--- Certificate-generator/certificate.js
+++ Certificate-generator/certificate.js
@@ -1,18 +1,31 @@
+var na = '';
 var form = document.getElementById("form");
-var month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-
-form.addEventListener('submit', function(event){
+form.addEventListener('submit', function (event) {
     event.preventDefault();
 
     var name = document.getElementById("name").value;
     var course = document.getElementById("course").value;
+    var start_date = document.getElementById('start-date').value;
+    var end_date = document.getElementById('end-date').value;
+
 
     document.getElementById('candidate_name').innerText = name;
     document.getElementById('subject').innerText = course;
 
-    var date = new Date();
-    document.querySelector('.month').innerText = month[date.getMonth()];
-    document.querySelector('.date').innerText = date.getDate();
-    document.querySelector('.year').innerText = date.getFullYear();
 
+    document.getElementById('startDate').innerText = start_date;
+    document.getElementById('endDate').innerText = end_date;
+    na = name.split(' ').join('');
 })
+
+
+$(document).ready(function () {
+    $('#btn').click(function () {
+        domtoimage.toBlob(document.getElementById('main')).then(function (blob) {
+            window.saveAs(blob, na + "-result.png");
+        })
+    })
+})
+
+
+
